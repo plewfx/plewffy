3dd259bea0e4bccbda250c1a7b5bcd4dfa0ffab2
Ali <plewffy@gmail.com>
2023-10-22 19:31:43 +0600

    add: skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { useQuery } from "convex/react";
+
 import { api } from "../convex/_generated/api";
 
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
   Card,
   CardContent,
@@ -12,36 +15,61 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Spinner } from "@/components/spinner";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { HelpCircle } from "lucide-react";
 
 import Link from "next/link";
 import Image from "next/image";
+
+import { techs } from "@/technologies.ts";
 
 export default function Home() {
   const projects = useQuery(api.projects.get);
 
   const renderedProjects = projects?.map((project) => (
     <li key={project._id}>
-      <Card>
-        <CardHeader>
-          <Image
-            src={project.img}
-            width={300}
-            height={100}
-            alt={project.name}
-            className="rounded-lg mb-5"
-          />
-          <CardTitle>{project.name}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <CardDescription>{project.description}</CardDescription>
-        </CardContent>
-        <CardFooter>
-          <Button variant="secondary" asChild>
-            <Link href={project.url}>Live app</Link>
-          </Button>
-        </CardFooter>
-      </Card>
+      <Tooltip>
+        <TooltipTrigger>
+          <Card>
+            <CardHeader>
+              <Image
+                src={"/projects" + project.img}
+                width={300}
+                height={100}
+                alt={project.name}
+                className="rounded-lg mb-5"
+              />
+              <CardTitle className="text-start">{project.name}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>{project.description}</CardDescription>
+            </CardContent>
+            <CardFooter>
+              <Button variant="secondary" asChild>
+                <Link href={project.url}>Live app</Link>
+              </Button>
+            </CardFooter>
+          </Card>
+        </TooltipTrigger>
+        <TooltipContent>
+          <ul className="flex items-center gap-2 flex-wrap">
+            {project.technologies.map((technology) => {
+              const tech = techs.find((t) => t.name === technology);
+              return (
+                <li key={technology} className="hover:text-primary">
+                  <Link href={tech.url}>{technology}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </TooltipContent>
+      </Tooltip>
     </li>
   ));
 
@@ -51,7 +79,7 @@ export default function Home() {
         <div className="mx-auto my-0 max-w-[85%]">
           <div className="flex flex-col items-center gap-5 text-center">
             <h1 className="text-5xl lg:text-6xl font-bold capitalize">
-              Young <span>front-end</span> developer <br /> form{" "}
+              Young <span>frontend</span> developer <br /> form{" "}
               <span>Kazakhstan</span>
             </h1>
             <p>
@@ -82,11 +110,42 @@ export default function Home() {
       <section className="py-10 lg:py-20" id="projects">
         <div className="mx-auto my-0 max-w-[85%]">
           <div className="flex flex-col items-center gap-7 lg:gap-10">
-            <h2 className="text-4xl lg:text-5xl font-bold capitalize text-center">
-              My <span>best projects</span>
-            </h2>
+            <div className="flex w-full items-center justify-center text-center relative">
+              <h2 className="text-4xl lg:text-5xl font-bold capitalize">
+                My <span>best projects</span>
+              </h2>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger className="absolute lg:right-[20%] lg:block hidden">
+                    <HelpCircle color="gray" />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>just hover a card to see the used technologies</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
             <ul className="flex flex-wrap">
-              {projects ? renderedProjects : <Spinner size="lg" />}
+              <TooltipProvider>
+                {projects ? (
+                  renderedProjects
+                ) : (
+                  <>
+                    <div className="w-[350px] h-[420px] border rounded-lg">
+                      <div className="flex flex-col space-y-1.5 p-6">
+                        <Skeleton className="w-[300px] h-[213px] mb-5" />
+                        <Skeleton className="w-[150px] h-[24px]" />
+                      </div>
+                      <div className="p-6 pt-0">
+                        <Skeleton className="w-[300px] h-[20px]" />
+                      </div>
+                      <div className="flex items-center p-6 pt-0">
+                        <Skeleton className="w-[100px] h-[20px]" />
+                      </div>
+                    </div>
+                  </>
+                )}
+              </TooltipProvider>
             </ul>
           </div>
         </div>
---
+++ technologies.ts
@@ -0,0 +1,19 @@
+export const techs = [
+    {
+        name: "Next.js",
+        url: "https://nextjs.org/",
+        icon: "/nextjs.svg",
+    },
+    {
+        name: "Tailwind CSS",
+        url: "https://tailwindcss.com/"
+    },
+    {
+        name: "shadcn/ui",
+        url: "https://ui.shadcn.com/"
+    },
+    {
+        name: "Figma",
+        url: "https://figma.com/"
+    },
+]
