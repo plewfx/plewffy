b803bdfbb5f38df5a0748472106ee492a91210af
Ali <plewffy@gmail.com>
2023-10-22 16:41:03 +0600

    add: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,32 @@
-import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
-import { ThemeProvider } from '@/components/theme-provider'
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 import ConvexClientProvider from "./convex-client-provider";
 
-import './globals.css'
-import { Navbar } from '@/components/navbar'
+import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
 
-const mont = Montserrat({ subsets: ['latin'] })
+import "./globals.css";
+
+const mont = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Plewffy',
-  description: 'The portfolio of front-end developer Plewffy',
+  title: "Plewffy",
+  description: "The portfolio of front-end developer Plewffy",
   icons: {
     icon: [
       {
         url: "/plewffy.svg",
         href: "/plewffy.svg",
-      }
-    ]
-  }
-}
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -37,12 +39,11 @@ export default function RootLayout({
         >
           <ConvexClientProvider>
             <Navbar />
-            <main className="mt-24">
-              {children}
-            </main>
+            <main className="mt-24">{children}</main>
+            <Footer />
           </ConvexClientProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "./ui/button";
+
+import { GithubIcon } from "lucide-react";
+
+export const Footer = () => {
+  return (
+    <footer className="py-5 lg:py-10">
+      <div className="mx-auto my-0 max-w-[85%]">
+        <div className="flex flex-col gap-10 lg:gap-20">
+          <div className="flex items-center justify-between">
+            <Image src="/plewffy.svg" width={50} height={50} alt="Plewffy" />
+            <Button size="icon" variant="outline" asChild>
+              <Link href="https://github.com/plewfx">
+                <GithubIcon />
+              </Link>
+            </Button>
+          </div>
+          <p className="text-foreground/50 text-center">© 2023 Plewffy</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
